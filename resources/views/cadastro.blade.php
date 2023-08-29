e020d190b1af81e724f99df898bb182f1a0583f0
Lucas Henrique Simon <lucassimon98@gmail.com>
2023-08-28 23:29:05 -0300

    Criado o projeto, criado a home e página de cadastro

---
+++ resources/views/cadastro.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts.app')
+
+@section('title', 'Cadastro')
+
+<link href="/css/index.css" rel="stylesheet" type="text/css">
+
+@section('content')
+<div class="background-inicio d-flex flex-column align-items-center justify-content-center">
+    <form action="" class="d-flex flex-column align-items-center bg-light w-50 p-5 rounded">
+        <div class="inputBox d-flex w-30 flex-column mb-3">
+            <label for="nome" class="labelInput font-bold">Nome Completo</label>
+            <input type="text" nome="nome" id="nome" class="inputUser" required>
+        </div>
+        <div class="inputBox d-flex w-30 flex-column mb-3">
+            <label for="senha" class="labelInput font-bold">Senha</label>
+            <input type="text" name="senha" id="senha" class="inputUser" required>
+        </div>
+        <div class="inputBox d-flex w-30 flex-column mb-3">
+            <label for="email" class="labelInput font-bold">Email</label>
+            <input type="text" name="email" id="email" class="inputUser" required>
+        </div>
+        <div class="inputBox d-flex w-25 flex-column">
+            <a href="#" class="btn btn-success">Cadastrar</a>
+        </div>
+    </form>
+</div>
+
+@endsection
---
+++ resources/views/index.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.app')
+
+@section('title', 'Inicio')
+
+<link href="/css/index.css" rel="stylesheet" type="text/css">
+
+
+@section('content')
+<div class="background-inicio d-flex flex-column justify-content-center">
+    <div class="container text-center align-middle">
+        <h1>Sistema Lucas Simon</h1>
+    </div>
+    <div class="row">
+        <div class="col-md-6 text-end">
+            <a href="#" class="btn btn-primary">Entrar</a>
+        </div>
+        <div class="col-md-6 text-start">
+            <a href="./cadastro" class="btn btn-primary">Cadastre-se</a>
+        </div>
+    </div>
+</div>
+
+@endsection
